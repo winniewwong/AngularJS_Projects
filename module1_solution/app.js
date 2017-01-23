0e2bfa993a2b13d18ee275a890e124401aff40ad
winniewwong <winniewwong@hotmail.com>
2017-01-23 02:46:50 -0500

    module1_solution

--- module1_solution/app.js
+++ module1_solution/app.js
@@ -4,8 +4,10 @@
 angular.module('LunchCheck', [])
 .controller('LunchCheckController', LunchCheckController);
 
-LunchCheckController.$inject = ['$scope', '$filter'];
-function LunchCheckController($scope, $filter) {
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.dishes = "";
+  $scope.message = "";
 
   $scope.CheckIfTooMuch = function () {
     // looks for 0 or more spaces followed by a comma followed by 0
@@ -13,25 +15,29 @@ function LunchCheckController($scope, $filter) {
     // the comma from the string
     var re = /\s*,\s*/;
     var splits = $scope.dishes.split(re);
+    var count = 0;
     console.log(splits);
-    $scope.count = 0;
 
     var i;
     for ( i = 0; i < splits.length; i++ ) {
       if ( splits[i] != "" ){
-        $scope.count++;
+        count++;
       }
     }
 
-    if ( $scope.dishes == "" || $scope.count == 0 ) {
+    console.log(count);
+
+    if ( $scope.dishes == "" || count == 0 ) {
         $scope.message = "Please enter data first";
     }
-    else if ( $scope.count <= 3 ){
+    else if ( count <= 3 ){
       $scope.message = "Enjoy!";
     }
-    else if ( $scope.count > 3 ){
+    else if ( count > 3 ){
       $scope.message = "Too much!";
     }
+
+
   };
 }
 
